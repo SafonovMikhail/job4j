9aded27a4f2439608e5ff18639588b160bc48823
Mikhail Safonov <ms33@inbox.ru>
2020-01-22 15:55:31 +0300

    4.5. Отладка программы в IDEA [#159464][159464#190720] (SqMax)

--- chapter_001/src/main/java/ru/job4j/condition/SqMax.java
+++ chapter_001/src/main/java/ru/job4j/condition/SqMax.java
@@ -1,21 +1,34 @@
 package ru.job4j.condition;
 
 public class SqMax {
-    public static int max(int first, int second, int third, int fourth) {
-        int result = fourth;
+    public static int max(int first, int second, int third, int forth) {
+        int result = forth;
+
         if (first > second) {
             if (first > third) {
-                if (first > fourth) {
+                if (first > forth) {
                     result = first;
                 }
+            } else if (third > forth) {
+                result = third;
             }
         } else if (second > third) {
-            if (second > fourth) {
+            if (second > forth) {
                 result = second;
             }
-        } else if (third > fourth) {
-            result = third;
+        } else if (third > forth) {
+            result = second;
         }
+
+//        if (first > second && first > third && first > forth) {
+//            result = first;
+//
+//        } else if (second > third && second > first && second > forth) {
+//            result = second;
+//
+//        } else if (third > first && third > second && third > forth) {
+//            result = third;
+//        }
         return result;
     }
 }
