acfaafb093e60d96f15616b82a6c4bb77105ec3a
Mikhail Safonov <ms33@inbox.ru>
2020-01-22 17:28:16 +0300

    4.5. Отладка программы в IDEA [#159464][159464#190720]

--- chapter_001/src/main/java/ru/job4j/loop/CheckPrimeNumber.java
+++ chapter_001/src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -1,11 +1,16 @@
 package ru.job4j.loop;
 
 public class CheckPrimeNumber {
-    public boolean check(int num) {
+    public static boolean check(int num) {
         boolean prime = false;
         if (num == 1)
             prime = false;
-        for (int d = 2; d * d <= num; d++) {
+        if (num == 2)
+            prime = true;
+        for (int d = 2; d < num; d++) {
+//       for (int d = 2; d <= num; d++) {
+//       for (int d = 2; d * d <= num; d++) {
+//          if ((double)num % (double)d == 0) {
             if (num % d == 0) {
                 prime = false;
                 break;
